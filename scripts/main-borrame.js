5ae2ea8dd505eb51259bc8ee98535a18a73e9b7e
Uanone <74508825+Uanone@users.noreply.github.com>
2021-01-31 12:32:17 +0100

    Add files via upload

---
+++ scripts/main-borrame.js
@@ -0,0 +1,10 @@
+
+let helado = "chocolate";
+if (helado==="chocolate"){
+    alert ("Si, amo el helado de chocolate");
+}   else {
+    alert ("Awww, pero mi favorito es el de chocolate");
+}
+
+let nombredelavariable= document.querySelector("h1")
+alert ("jllal")
